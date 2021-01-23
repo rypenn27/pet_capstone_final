7222da590f370bebf224b11a76fc289d7dbffba7
Kaitlin Bray <Kaitlin.bray@yahoo.com>
2021-01-23 10:37:37 -0500

    Implemented Search

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,17 @@ import React, { useState, useEffect } from 'react';
 
 import { getSomething } from '../api';
 
-const Header = () => {
+const Header = (props) => {
+  const pets = props.pets;
+  const setPets = props.setPets;
+
   return (
-    <>
-      <Search />
-    </>
+    <header>
+      <h1 className='logo'>Pet Rescuers</h1>
+      <h5>Find Your Forever Friend!</h5>
+
+      <Search pets={pets} setPets={setPets} />
+    </header>
   );
 };
 
