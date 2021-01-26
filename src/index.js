d9f6c758687fc91975ca53b5629fd20f0ef5d355
Ryan Pennington <rypenn27@yahoo.com>
2021-01-25 20:23:18 -0500

    updating for kate branch

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 7222da590f370bebf224b11a76fc289d7dbffba7
 
-import { App } from "./components";
+import { App } from './components';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
